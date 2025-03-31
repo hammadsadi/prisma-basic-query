424b9f9d918417f47b052a2140c0b699e0600ab2
hammadsadi <hammad.sadi@yahoo.com>
2025-04-01 00:11:47 +0600

    Many to many Relation query updated

--- src/relation.ts
+++ src/relation.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, UserRole } from "@prisma/client";
 
 const prisma = new PrismaClient()
 
@@ -6,19 +6,19 @@ const main = async() =>{
     // Create User
     // const createduser = await prisma.user.create({
     //     data:{
-    //         username:"Zakiya",
-    //         email:"zakiya@yahoo.com"
+    //         username:"Sayyid Saadi",
+    //         email:"sayyidsaadi@yahoo.com",
+    //         role: UserRole.admin
     //     }
     // })
 
     // create Profile bio
     // const createdProfileBio = await prisma.profile.create({
     //     data:{
-    //         bio:"Im Mern Stack Developer",
-    //         userId:1
+    //         bio:"Full Stack Developer",
+    //         userId:4
     //     }
     // })
-
 
     //  Create Category
     // const createdCategory = await prisma.category.create({
@@ -28,7 +28,21 @@ const main = async() =>{
     // })
 
     // Create Post
- 
+    // const createdPost = await prisma.post.create({
+    //     data:{
+    //         title:"Node  Js Developer",
+    //         content: 'Im Node JS Developer and Full Stack Developer',
+    //         authorId: 4,
+    //         postCategory:{
+    //             create:{
+    //                categoryId: 2
+    //             }
+    //         }
+    //     },
+    //     include:{
+    //         postCategory:true
+    //     }
+    // })
 
 }
 main()
