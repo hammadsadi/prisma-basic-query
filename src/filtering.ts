ea683ae165336cf0ca473badd78aecf163873798
hammadsadi <hammad.sadi@yahoo.com>
2025-04-03 00:15:21 +0600

    More Advance Filtering Query Added

--- src/filtering.ts
+++ src/filtering.ts
@@ -21,21 +21,91 @@ const filtering = async () =>{
 
 
     //  Or Filtering
-    const ordFiltering = await prisma.post.findMany({
+    // const ordFiltering = await prisma.post.findMany({
+    //     where:{
+    //         OR:[
+    //             {
+    //                 title:{
+    //                     contains: 'Jvascript'
+    //                 }
+    //             },
+    //             {
+    //                 published:true
+    //             }
+    //         ]
+    //     }
+    // })
+
+
+    //  Not Filtering 
+    const notFiltering = await prisma.post.findMany({
         where:{
-            OR:[
+            NOT:[
                 {
                     title:{
                         contains: 'Jvascript'
                     }
-                },
-                {
-                    published:true
                 }
             ]
         }
     })
-    console.log(ordFiltering)
+
+    //  Start With
+    const startWithFilter = await prisma.user.findMany({
+        where:{
+            email:{
+                startsWith: 'ha'
+            }
+        }
+    })
+
+    //  End With
+    const endWithFilter = await prisma.user.findMany({
+        where:{
+            username:{
+                endsWith:'di'
+            }
+        }
+    })
+
+    //  Equals
+    const equalsFilter = await prisma.user.findMany({
+        where:{
+            role:{
+                equals:'admin'
+            }
+        }
+    })
+
+    // In
+    const userNameList = ['Sayyid Saadi', 'Hammad Sadi', 'customer']
+    const infilter = await prisma.user.findMany({
+        where:{
+            username:{
+                in: userNameList
+            }
+        }
+    })
+
+
+    //  In Depth Relation Data Get
+    const inDepthRelationData = await prisma.user.findUnique({
+        where:{
+            id:4
+        },
+        include:{
+            post:{
+                include:{
+                    postCategory:{
+                        include:{
+                            category:true
+                        }
+                    }
+                }
+            }
+        }
+    })
+    console.dir(inDepthRelationData, {depth: Infinity})
 
   
 }
